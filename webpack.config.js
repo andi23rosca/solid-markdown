943b1168aedc4bcf3a3ee92bdf5a18dcc179f4b8
Andi <andi23rosca@gmail.com>
2021-10-31 09:59:09 +0100

    Added high1 to contributors

--- webpack.config.js
+++ webpack.config.js
@@ -1,36 +1,43 @@
-const path = require('path');
-const TerserWebpackPlugin = require('terser-webpack-plugin');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const path = require("path");
+const TerserWebpackPlugin = require("terser-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
-module.exports = (_, { mode = 'development' }) => ({
-  target: 'web',
+module.exports = (_, { mode = "development" }) => ({
+  target: "web",
   entry: {
-    index: './lib/index.tsx',
+    index: "./lib/index.tsx",
   },
   output: {
-    path: path.resolve(__dirname, './dist'),
-    filename: 'index.js',
-    library: 'solid-markdown',
-    libraryTarget: 'umd',
-    globalObject: 'this',
+    path: path.resolve(__dirname, "./dist"),
+    filename: "index.js",
+    library: "solid-markdown",
+    libraryTarget: "umd",
+    globalObject: "this",
     umdNamedDefine: true,
   },
   externals: {
-    'solid-js': 'solid-js',
+    "solid-js": "solid-js",
   },
-  plugins: [new CleanWebpackPlugin(), ...(mode === 'production' ? [new TerserWebpackPlugin()] : [])],
+  plugins: [
+    new CleanWebpackPlugin(),
+    ...(mode === "production"
+      ? [
+          new TerserWebpackPlugin({
+            extractComments: false,
+          }),
+        ]
+      : []),
+  ],
   module: {
     rules: [
       {
         test: /\.tsx?$/,
         exclude: [/node_modules/],
-        use: [
-          'babel-loader'
-        ],
+        use: ["babel-loader"],
       },
     ],
   },
   resolve: {
-    extensions: ['.tsx', '.ts', '.js'],
+    extensions: [".tsx", ".ts", ".js"],
   },
 });
