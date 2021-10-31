5f55ba92c524d09242d51c267398d46d3171224f
high1 <radomir.simic@gmail.com>
2021-10-31 09:21:06 +0100

    Use Webpack to build the package
    - package could not be used when built by rollup, micromark depends
    on debug and rollup has a long standing issue with debug
    - created webpack configuration
    - added babel configuration
    - use ts-loader to extract type definitions
    - updated all packages
    - removed pnpm lock file

---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+const path = require('path');
+const TerserWebpackPlugin = require('terser-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = (_, { mode = 'development' }) => ({
+  target: 'web',
+  entry: {
+    index: './lib/index.tsx',
+  },
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    filename: 'index.js',
+    library: 'solid-markdown',
+    libraryTarget: 'umd',
+    globalObject: 'this',
+    umdNamedDefine: true,
+  },
+  externals: {
+    'solid-js': 'solid-js',
+  },
+  plugins: [new CleanWebpackPlugin(), ...(mode === 'production' ? [new TerserWebpackPlugin()] : [])],
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        exclude: [/node_modules/],
+        use: [
+          'babel-loader', 'ts-loader'
+        ],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+});
