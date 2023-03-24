d1124caa59421076820299339a849ffd13f1c99b
Patrick Heneise <patrick@zentered.co>
2023-03-24 10:27:22 +0200

    chore: prettier

--- example/vite.config.ts
+++ example/vite.config.ts
@@ -1,10 +1,10 @@
-import { defineConfig } from "vite";
-import solidPlugin from "vite-plugin-solid";
+import { defineConfig } from 'vite'
+import solidPlugin from 'vite-plugin-solid'
 
 export default defineConfig({
   plugins: [solidPlugin()],
   build: {
-    target: "esnext",
-    polyfillDynamicImport: false,
-  },
-});
+    target: 'esnext',
+    polyfillDynamicImport: false
+  }
+})
