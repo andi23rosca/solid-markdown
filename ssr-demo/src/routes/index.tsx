9aa6a750458fcb9ccef639683e910447ea5e3d97
Andi Rosca <andi23rosca@gmail.com>
2023-10-31 11:21:28 +0100

    SSR demo to ensure the renderer works in node

---
+++ ssr-demo/src/routes/index.tsx
@@ -0,0 +1,53 @@
+// solid-refresh
+import { createSignal, type Component, createEffect } from "solid-js";
+import remarkGfm from "remark-gfm";
+import { SolidMarkdown } from "solid-markdown";
+
+const initial = `# 🚀 solid-markdown demo
+
+Edit any of the text and see it rendered in real time.
+
+A paragraph with *emphasis* and **strong importance**.
+
+## Custom component for heading 2
+
+> A block quote with ~strikethrough~ and a URL: https://reactjs.org.
+
+* Lists
+* [ ] todo
+* [x] done
+	1. nested
+	2. lists
+* another one
+
+  nested text under a list item
+
+A table:
+
+| Syntax      | Description | Test Text     |
+| :---        |    :----:   |          ---: |
+| Header      | Title       | Here's this   |
+| Paragraph   | Text        | And more      |`;
+
+const App: Component = () => {
+	const [md, setMd] = createSignal(initial);
+
+	return (
+		<div class="container">
+			<textarea
+				rows={md().split(/\r\n|\r|\n/).length + 2}
+				class="editor"
+				onInput={(e) => {
+					setMd(e.currentTarget.value);
+				}}
+				contentEditable
+			>
+				{md()}
+			</textarea>
+
+			<SolidMarkdown remarkPlugins={[remarkGfm]}>{md()}</SolidMarkdown>
+		</div>
+	);
+};
+
+export default App;
