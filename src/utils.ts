f8a614a00ac5117e4815c9ed5104e98dad688763
Andi Rosca <andi23rosca@gmail.com>
2024-03-19 20:01:55 +0100

    Fix: Style properties should not be camel case

--- src/utils.ts
+++ src/utils.ts
@@ -2,7 +2,6 @@ import { stringify as commas } from "comma-separated-tokens";
 import type { Root, Element } from "hast";
 import { find } from "property-information";
 import { stringify as spaces } from "space-separated-tokens";
-import style from "style-to-object";
 import { Position } from "unist";
 import { Context } from "./types";
 
@@ -42,6 +41,11 @@ export function addProperty(
 	const info = find(ctx.schema, prop);
 	let result = value;
 
+
+	if (info.property === "className") {
+		info.property = 'class'
+	}
+
 	// Ignore nullish and `NaN` values.
 	// eslint-disable-next-line no-self-compare
 	// biome-ignore lint/suspicious/noSelfCompare: <explanation>
@@ -55,42 +59,19 @@ export function addProperty(
 		result = info.commaSeparated ? commas(result) : spaces(result);
 	}
 
-	if (info.property === "style" && typeof result === "string") {
-		result = parseStyle(result);
-	}
-
 	if (info.space && info.property) {
 		props[info.property] = result;
 	} else if (info.attribute) {
 		props[info.attribute] = result;
 	}
 }
-function parseStyle(value: string): Record<string, string> {
-	const result: Record<string, string> = {};
-
-	try {
-		style(value, iterator);
-	} catch {
-		// Silent.
-	}
-
-	return result;
-
-	function iterator(name: string, v: string) {
-		const k = name.slice(0, 4) === "-ms-" ? `ms-${name.slice(4)}` : name;
-		result[k.replace(/-([a-z])/g, styleReplacer)] = v;
-	}
-}
-function styleReplacer(_: unknown, $1: string) {
-	return $1.toUpperCase();
-}
 export function flattenPosition(
 	pos:
 		| Position
 		| {
-				start: { line: null; column: null; offset: null };
-				end: { line: null; column: null; offset: null };
-		  },
+			start: { line: null; column: null; offset: null };
+			end: { line: null; column: null; offset: null };
+		},
 ): string {
 	return [
 		pos.start.line,
